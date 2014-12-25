3292c89a3fb0b375ef46886a0436df074591904b
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-12-25 17:30:08 +0000

    Added basic elasticsearch module documentation. Added logic for checking if the plugin's configuration is available.

---
+++ server/plugins/elastricsearch/README.md
@@ -0,0 +1,10 @@
+This module provides logic for persisting tail data into elasticsearch.
+
+To enable it add the following configuration entry to your configuration file, in the [plugins] key.
+
+plugin[] = 'elastricsearch'
+
+This module expects the following configuration values to be made available:
+
+[elasticsearch]
+TODO: Add documentation.
--- server/plugins/elastricsearch/module.js
+++ server/plugins/elastricsearch/module.js
@@ -1,5 +1,24 @@
-// Export event handlers.
-module.exports = function(serverInstance) {
+// Stores an instance of the server.
+var serverInstance;
+
+// Loads module specific configuration.
+function loadModuleConfiguration() {
+    // Check if configuration is available.
+    if (typeof serverInstance.config.elasticsearch === "undefined") {
+        console.log("Can not load 'elasticsearch' plugin configuration. Process terminated.");
+        process.exit(1);
+    }
+}
+
+// Instantiates the module.
+module.exports = function(server) {
+    // Store the server instance.
+    serverInstance = server;
+
+    // Initialise plugin.
+    loadModuleConfiguration();
+
+    // Return event handlers.
     return {
             // Agent specific event handlers.
             agent: {
